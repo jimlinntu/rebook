26ea8eb6ff6eb7ec16ebab1dcc8c985b6279b711
Jim <b04705003@ntu.edu.tw>
2020-06-05 16:38:06 +0800

    collate.pyx: add comments

--- collate.pyx
+++ collate.pyx
@@ -5,6 +5,7 @@ cimport numpy as np
 
 cimport cython
 
+# Initial a C class for internal use
 cdef class CLetter:
     cdef np.ndarray label_map, stats, centroid
     cdef int label, x, y, w, h, r, b
@@ -21,6 +22,7 @@ cdef class CLetter:
         self.r = self.x + self.w
         self.b = self.y + self.h
 
+    # Convert an internal C class back to Python class object
     def letter(self):
         return Letter(self.label, self.label_map, self.stats, self.centroid)
 
@@ -29,10 +31,14 @@ def collate_lines(int AH, list letters):
     cdef CLetter last1, last2, cl, letter, first, last
     cdef list lines, best_candidate
 
+    # Convert Python object into C object
     letters = [CLetter(l) for l in letters]
+    # Sort from left to right
     letters.sort(key=lambda CLetter cl: cl.x)
 
     lines = []
+    # Idea: for each letter, find a best line for it
+    #       if we cannot find a suitable line for it, create a line start from this letter
     for letter in letters:
         best_candidate = []
         best_score = 100000
@@ -41,9 +47,17 @@ def collate_lines(int AH, list letters):
             last1 = line[-1]
             last2 = line[-2] if line_len > 1 else last1
             score = best_score
+            # if this letter is not too far from last1 in terms of x coordinate
+            # and last1 should be on top of letter.b+3
+            # and letter should be on top of last
+            # (Intuition, letter and last's ys should not be too far away)
             if letter.x < last1.r + 4 * AH \
                     and last1.y <= letter.b + 3 and letter.y <= last1.b + 3:
+                # Use Mahattan distance
                 score = letter.x - last1.r + abs(letter.y - last1.y)
+            # if a letter does not pass the condition above, we give it a second chance
+            # if this letter's x coordinates is not too far from last2
+            # and letter is on top of last2 and last2 is on top of letter
             elif line_len > 1 \
                     and letter.x < last2.x + last2.w + AH \
                     and last2.y <= letter.b and letter.y <= last2.b:
@@ -55,6 +69,7 @@ def collate_lines(int AH, list letters):
         if best_candidate:
             first = best_candidate[0]
             last = best_candidate[-1]
+            # if there exists a suitable line for this letter, append this letter behind this line
             best_candidate.append(letter)
             if letter.x - last.x > 300 or letter.x < first.x or letter.x - first.x > 2000:
                 print "agggghhh"
@@ -63,6 +78,7 @@ def collate_lines(int AH, list letters):
                 print "new  ", letter.letter()
             # print "  selected:", x, y, w, h
         else:
+            # otherwise, create a new line starting from this letter
             lines.append([letter])
 
     return [TextLine([cl.letter() for cl in line]) for line in lines]
